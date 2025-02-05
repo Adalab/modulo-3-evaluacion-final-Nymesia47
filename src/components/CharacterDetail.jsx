c34e618db8a4b9b36128bc2c8ed2c79c41fee437
Gabriella Calvano <gabcalvano@gmail.com>
2025-02-05 14:08:03 +0100

    added icon and completed change icon functionality

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,39 +1,51 @@
 import PropTypes from "prop-types";
+import '../styles/CharacterDetail.scss';
 import translations from "../services/translations";
 import { useEffect, useState } from "react";
+import { FaHeartPulse, FaSkull, FaUser, FaQuestion } from "react-icons/fa6";
+import {GiGiant , GiWerewolf } from "react-icons/gi";
+import {TbGhost2Filled} from "react-icons/tb";
 
 function CharacterDetail({data, useCharacterImage}) {
 
     const {imgUrl, altText} = useCharacterImage(data);
+    const speciesIcons = {
+        "human": <FaUser/>,
+        "half-giant": <GiGiant />,
+        "werewolf": <GiWerewolf/>,
+        "ghost": <TbGhost2Filled/>
+    };
 
     let [status, setStatus] = useState("");
-
-    const changesStatus = (data)=> {
-        if (data.alive && data.gender === "female") {
-            setStatus("Viva")
-        } else if (data.alive && data.gender === "male") {
-            setStatus("Vivo")
-        } else if (!data.alive && data.gender === "female") {
-            setStatus("Muerta")
-        } else if (!data.alive && data.gender === "male") {
-            setStatus("Muerto")
-        } else {
-            setStatus("Desconocido")
-        }
-
-        return status;
-    }
+    
 
     useEffect(() => {
+        const changesStatus = (data)=> {
+            if (data.alive && data.gender === "female") {
+                setStatus("Viva")
+            } else if (data.alive && data.gender === "male") {
+                setStatus("Vivo")
+            } else if (!data.alive && data.gender === "female") {
+                setStatus("Muerta")
+            } else if (!data.alive && data.gender === "male") {
+                setStatus("Muerto")
+            } else {
+                setStatus("Desconocido")
+            }
+    
+            return status;
+        }
         changesStatus(data);
-      }, [data]);
+      }, [data, status]);
     
   return (
    <article>
     <img src={imgUrl} alt={altText} />
     <p>{data.name}</p>
-    <p>Estatus: {status}</p>
-    <p>Especie: {translations[data.species] || data.species}</p>
+    <p>Estatus: {status}{" "}
+      {data.alive ? <FaHeartPulse className="heart-icon"/> : <FaSkull />}
+    </p>
+    <p>Especie: {translations[data.species] || data.species}{" "}{speciesIcons[data.species] || <FaQuestion/>}</p>
     <p>Género: {translations[data.gender]}</p>
     <p>Casa: {data.house}</p>
 
